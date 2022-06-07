35bc07c8f5bd0f5c5a39cee7e1aa1282974e5b59
Roharui <jack555586@gmail.com>
2022-06-07 22:23:05 +0900

    build: add .env config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { NovelInfoEntity } from './entity/novel-info.entity';
 import { NovelEntity } from './entity/novels.entity';
@@ -6,7 +7,19 @@ import { NovelModule } from './novel/novel.module';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      envFilePath: ['.env', '.env.production'],
+      isGlobal: true,
+    }),
     TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT) || 5432,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
+      synchronize: true,
+      logging: true,
       entities: [NovelEntity, NovelInfoEntity],
     }),
     NovelModule,
